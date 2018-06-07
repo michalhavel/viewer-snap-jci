4a7eb3f3a95ba4b863aa62f7ffa26f18072c81af
Michal Havel <michal.havel.pdy@seznam.cz>
2018-06-07 08:52:56 +0200

    Scope internal, API version to v2

--- server/credentials_.js
+++ server/credentials_.js
@@ -25,12 +25,13 @@ var credentials ={
 		client_secret: process.env.FORGE_CLIENT_SECRET || 'E4QmjoZixFZStT6J',
 		grant_type: 'client_credentials',
 		scope: 'viewables:read', // Setup the needed scopes for authorizing your Token
-		callbackUrl: process.env.CALLBACK_URL || 'https://cadsforge-react.herokuapp.com//api/forge/callback/oauth'
+		scopeInternal: ['data:read','data:write','data:create','data:search'],
+		callbackUrl: process.env.CALLBACK_URL || 'https://cadsforge-react.herokuapp.com/api/forge/callback/oauth'
 	},
 	
 	// If you which to use the Autodesk View & Data API on the staging server, change this url
 	BaseUrl: 'https://developer.api.autodesk.com',
-	Version: 'v1'
+	Version: 'v2'
 } ;
 
 credentials.Authentication =credentials.BaseUrl + '/authentication/' + credentials.Version + '/authenticate'
